6457de1c5c8957271d58f00cde076e62884b1530
Zander Martineau <zander@martineau.tv>
2017-11-05 16:12:17 +0000

    increase test converage

--- src/calcs.test.js
+++ src/calcs.test.js
@@ -8,5 +8,7 @@ test('multiply', () => {
 test(`pxTo`, () => {
   expect(pxTo(30, 16, 'em')).toBe('1.875em')
   expect(pxTo(30, 16, 'rem')).toBe('1.875rem')
+  expect(pxTo(30, 16)).toBe('1.875rem')
+  expect(pxTo(30)).toBe('1.5rem')
   expect(pxTo(30, 16, 'px')).toBe('1.875px')
 })
