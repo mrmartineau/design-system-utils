3b871acfb97af56f7833604ede36bbf13c6fb7cb
Zander Martineau <zander@martineau.tv>
2018-04-06 23:57:45 +0100

    fix type definition for `get` method

--- src/index.d.ts
+++ src/index.d.ts
@@ -9,7 +9,7 @@ export default class DesignSystem {
   multiply(initial: number, multiplier: number): any;
 
   /*~ get a value from the design system object */
-  get(obj: object, val: string): any;
+  get(val: string, obj?: object): any;
 
   /*~ get a breakpoint value from the design system object */
   bp(bp: string): any;
